3f4938beaaee562365f956e962065831f8dd38d0
Karthik <karthik131100@gmail.com>
2022-10-16 21:26:18 +0530

    added time taken

--- Search_Algorithms/Linear Search/linear_search.py
+++ Search_Algorithms/Linear Search/linear_search.py
@@ -1,6 +1,7 @@
 # Linear Search
 # T.C. Best case :- O(1) - Constant time
 #      Worst case:- O(n)
+from datetime import datetime
 
 cars = [4,3,2,7,6,9,1]
 
@@ -16,5 +17,12 @@ def find_car(current_parking, current_car):
             #print(f"car not found at position: {current_parking.index(i)+1}")
             pass
 
-find_car(cars, 7)
 
+
+start = datetime.now()
+find_car(cars, 7)
+end = datetime.now()
+
+time_difference = end - start
+
+print("Time taken :: ",time_difference.total_seconds()," seconds")
